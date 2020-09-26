456f43a8b850935767a4bee0e6f7438997349db1
Mateus <mateustrindade8@gmail.com>
2020-09-26 18:53:48 -0300

    Inclusão da parte E

--- src/br/application/desafio/Curso.kt
+++ src/br/application/desafio/Curso.kt
@@ -1,10 +1,15 @@
 package br.application.desafio
 
-class Curso(var nome: String, var codigoCurso: Int) {
+class Curso(var nome: String, var codigoCurso: Int, var professorTitular: ProfessorTitular, var professorAdjunto: ProfessorAdjunto, var qdtMaxAlunos: Int, var alunos: MutableList<Aluno>) {
 
     override fun equals(other: Any?): Boolean { return other is Curso && codigoCurso == other.codigoCurso }
 
-    override fun toString(): String { return "Nome: $nome || Código: $codigoCurso"  }
+    override fun toString(): String {
+
+        var string = "Nome: $nome || Código: $codigoCurso || Professor Titular: $professorTitular || Professor Adjunto: $professorAdjunto \n Alunos da Turma: \n"
+        alunos.forEach(){string += it.toString() + "\n"}
+        return string
+    }
 
     override fun hashCode(): Int { return codigoCurso }
 
