add2b101141d390e9c6c5b55c394ec60cc00cbe2
Mateus <mateustrindade8@gmail.com>
2020-09-25 21:05:37 -0300

    Inclusão parte A

---
+++ src/br/application/desafio/Aluno.kt
@@ -0,0 +1,9 @@
+package br.application.desafio
+
+class Aluno(var nome: String, var sobrenome: String, var codigoAluno: Int) {
+
+    override fun equals(other: Any?): Boolean { return other is Aluno && codigoAluno == other.codigoAluno }
+
+    override fun toString(): String { return "Nome: $nome || Sobrenome: $sobrenome || Código: $codigoAluno"   }
+
+}
