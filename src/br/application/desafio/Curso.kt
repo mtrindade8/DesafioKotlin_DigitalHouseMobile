712de6cd7962395ea5946b0a9f105df88994ddb0
Mateus <mateustrindade8@gmail.com>
2020-09-27 19:49:15 -0300

    Inclusão da parte H

--- src/br/application/desafio/Curso.kt
+++ src/br/application/desafio/Curso.kt
@@ -2,17 +2,39 @@ package br.application.desafio
 
 class Curso(var nome: String, var codigoCurso: Int, var professorTitular: ProfessorTitular, var professorAdjunto: ProfessorAdjunto, var qdtMaxAlunos: Int) {
 
-    lateinit var alunos: MutableSet<Aluno>
+    lateinit var alunos: MutableMap<Int, Aluno>
 
     override fun equals(other: Any?): Boolean { return other is Curso && codigoCurso == other.codigoCurso }
 
     override fun toString(): String {
 
         var string = "Curso: $nome || Código: $codigoCurso || Professor Titular: ${professorTitular.nome+" "+professorTitular.sobrenome}  || Professor Adjunto: ${professorAdjunto.nome+" "+professorAdjunto.sobrenome} \nAlunos da Turma: \n"
-        alunos.forEach(){string += "\t" + it.toString() + "\n"}
+        alunos.forEach(){string += "\t" + it.value.toString() + "\n"}
         return string
     }
 
     override fun hashCode(): Int { return codigoCurso }
+
+    fun adicionarUmAluno(umAluno: Aluno):Boolean{
+
+        when {
+            alunos.count() < qdtMaxAlunos -> {
+                alunos.put(umAluno.hashCode(), umAluno)
+                return true
+            }
+            else -> return false
+        }
+    }
+
+    fun excluirAluno(umAluno: Aluno):Boolean{
+
+        when {
+            alunos.containsKey(umAluno.hashCode()) -> {
+                alunos.remove(umAluno.hashCode())
+                return true
+            }
+            else -> return false
+        }
+    }
 
 }
