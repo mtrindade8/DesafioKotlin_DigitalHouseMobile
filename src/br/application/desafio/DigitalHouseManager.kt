1710e32ee6e0c453b5ccd2ea160f6ec5c24580db
Mateus <mateustrindade8@gmail.com>
2020-09-27 19:05:04 -0300

    Inclusão da parte G

---
+++ src/br/application/desafio/DigitalHouseManager.kt
@@ -0,0 +1,12 @@
+package br.application.desafio
+
+class DigitalHouseManager(
+        var listaAlunos: MutableSet<Aluno>,
+        var listaProfessores: MutableSet<Professor>,
+        var listaCursos: MutableSet<Curso>,
+        var listaMatriculas: MutableSet<Matricula>
+) {
+
+
+
+}
